34b90e44d5d088d9995af4bd0ceffa597c4d0fc6
Penguinum <penguinum@gmail.com>
2021-01-01 15:40:01 +0300

    Added overrided binding for sv_audio_callback

--- overrides.lua
+++ overrides.lua
@@ -25,6 +25,36 @@ static int lua_sv_get_time_map(lua_State* L) {
     lua_settable(L, -4);
   }
   lua_pushnumber(L, lua_ret);
+  free(lua_dest);
+
+  return 1;
+}
+]]
+
+overrides.sv_audio_callback = [[
+static int lua_sv_audio_callback(lua_State* L) {
+
+  int lua_frames = (int)luaL_checknumber(L, 2);
+  int lua_latency = (int)luaL_checknumber(L, 3);
+  uint32_t lua_out_time = (uint32_t)luaL_checknumber(L, 4);
+
+  #ifdef SV_INIT_FLAG_AUDIO_FLOAT32
+  float* lua_buf = (float*)calloc(lua_frames, sizeof(float));
+  #else
+  int16_t* lua_buf = (int16_t*)calloc(frames, sizeof(int16_t));
+  #endif
+  // lua stack: {}, frames, latency, out_time
+
+  int lua_ret = sv_audio_callback(lua_buf, lua_frames, lua_latency, lua_out_time);
+
+  for (int i = 0; i < lua_frames; i++) {
+    lua_pushnumber(L, i + 1);
+    lua_pushnumber(L, lua_buf[i]);
+    lua_settable(L, 1);
+  }
+
+  lua_pushnumber(L, lua_ret);
+  free(lua_buf);
 
   return 1;
 }
