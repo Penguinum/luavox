0a70bee956097ebee82bcff20f7942d7f69953a6
Penguinum <penguinum@gmail.com>
2021-01-01 19:51:56 +0300

    Added override for sv_get_module_inputs

--- overrides.lua
+++ overrides.lua
@@ -111,4 +111,26 @@ static int lua_sv_audio_callback2(lua_State* L) {
 }
 ]]
 
+overrides.sv_get_module_inputs = [[
+static int lua_sv_get_module_inputs(lua_State* L) {
+  int slot = (int)luaL_checknumber(L, 1);
+  int mod_num = (int)luaL_checknumber(L, 2);
+
+  uint32_t module_flags = sv_get_module_flags(slot, mod_num);
+
+  int* ret = sv_get_module_inputs(slot, mod_num);
+
+  size_t number_of_inputs = (module_flags & SV_MODULE_INPUTS_MASK) >> SV_MODULE_INPUTS_OFF;
+
+  lua_newtable(L);
+  for (int i = 0; i < number_of_inputs; i++) {
+    lua_pushnumber(L, i + 1);
+    lua_pushnumber(L, ret[i]);
+    lua_settable(L, -3);
+  }
+
+  return 1;
+}
+]]
+
 return overrides
