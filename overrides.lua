2b7b48383fc79652fa293d4837610302a1f24970
Penguinum <penguinum@gmail.com>
2021-01-02 10:48:01 +0300

    Simplify overrides.lua

--- overrides.lua
+++ overrides.lua
@@ -1,18 +1,18 @@
 local overrides = {}
 
-local function read_c_file(filename)
-  local f = io.open("overrides/" .. filename .. ".c", "r")
-  local content = f:read("*all")
+local function register_override(func_name)
+  local f = io.open("overrides/" .. func_name .. ".c", "r")
+  local file_content = f:read("*all")
   f:close()
-  return content
+  overrides[func_name] = file_content
 end
 
-overrides.sv_get_time_map = read_c_file("sv_get_time_map")
-overrides.sv_audio_callback = read_c_file("sv_audio_callback")
-overrides.sv_audio_callback2 = read_c_file("sv_audio_callback2")
-overrides.sv_get_module_inputs = read_c_file("sv_get_module_inputs")
-overrides.sv_get_module_outputs = read_c_file("sv_get_module_outputs")
-overrides.sv_get_module_scope2 = read_c_file("sv_get_module_scope2")
-overrides.sv_module_curve = read_c_file("sv_module_curve")
+register_override("sv_get_time_map")
+register_override("sv_audio_callback")
+register_override("sv_audio_callback2")
+register_override("sv_get_module_inputs")
+register_override("sv_get_module_outputs")
+register_override("sv_get_module_scope2")
+register_override("sv_module_curve")
 
 return overrides
