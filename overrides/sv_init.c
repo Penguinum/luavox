cadd0baab1650f7b4e101e41ace228049e8c5966
Penguinum <penguinum@gmail.com>
2021-01-08 22:27:41 +0300

    Change sv_init so that flags are passed as Lua table

--- overrides/sv_init.c
+++ overrides/sv_init.c
@@ -1,14 +1,80 @@
+static int is_flag_set_true(lua_State *L, int nparam, const char *key) {
+  lua_pushstring(L, key);
+  lua_gettable(L, nparam);
+  int ltype = lua_type(L, -1);
+  if (ltype == LUA_TNIL) {
+    return 0;
+  } else if (ltype == LUA_TBOOLEAN) {
+    return lua_toboolean(L, -1);
+  } else {
+    return 1;
+  }
+}
+
+static int get_int_param(lua_State *L, int nparam, const char *key) {
+  lua_pushstring(L, key);
+  lua_gettable(L, nparam);
+  int ltype = lua_type(L, -1);
+  if (ltype == LUA_TNIL) {
+    return 0;
+  } else if (ltype == LUA_TNUMBER) {
+    return luaL_checkinteger(L, -1);
+  } else {
+    luaL_error(L, "Bad parameter type for '%s': expected number, got %s\n", key, lua_typename(L, ltype));
+    return 0; // won't get there tbh
+  }
+}
+
+/*
+From SunVox docs:
+
+SV_INIT_FLAG_NO_DEBUG_OUTPUT;
+SV_INIT_FLAG_USER_AUDIO_CALLBACK - offline mode: system-dependent audio stream will not be created; user must call sv_audio_callback() to get the next piece of sound stream;
+SV_INIT_FLAG_OFFLINE - same as SV_INIT_FLAG_USER_AUDIO_CALLBACK;
+SV_INIT_FLAG_AUDIO_INT16 - desired sample type of the output sound stream : int16_t;
+SV_INIT_FLAG_AUDIO_FLOAT32 - desired sample type of the output sound stream : float; the actual sample type may be different, if SV_INIT_FLAG_USER_AUDIO_CALLBACK is not set;
+SV_INIT_FLAG_ONE_THREAD - audio callback and song modification are in single thread; use it with SV_INIT_FLAG_USER_AUDIO_CALLBACK only.
+*/
+static int get_flags(lua_State *L, int nparam) {
+  int flags = 0;
+  if (is_flag_set_true(L, nparam, "no_debug_output")) {
+    flags |= SV_INIT_FLAG_NO_DEBUG_OUTPUT;
+  }
+  if (is_flag_set_true(L, nparam, "offline")) {
+    flags |= SV_INIT_FLAG_OFFLINE;
+  }
+  // We don't cover "user_audio_callback" as it's the same as "offline"
+  if (is_flag_set_true(L, nparam, "one_thread")) {
+    flags |= SV_INIT_FLAG_ONE_THREAD;
+  }
+
+  int sample_type = get_int_param(L, nparam, "sample_type");
+  if (sample_type == 16) {
+    use_int16_t = 1;
+    flags |= SV_INIT_FLAG_AUDIO_INT16;
+  } else if (sample_type == 32) {
+    use_int16_t = 0;
+    flags |= SV_INIT_FLAG_AUDIO_FLOAT32;
+  } else if (sample_type != 0) {
+    luaL_error(L, "Parameter \"sample_type\" can be either 16 or 32, not %d\n", sample_type);
+  }
+  return flags;
+}
+
 static int lua_sv_init(lua_State *L) {
   const char* config = luaL_checkstring(L, 1);
   int freq = luaL_checkinteger(L, 2);
   int channels = luaL_checkinteger(L, 3);
-  uint32_t flags = check_unsigned_int(L, 4);
+  // Let's suppose we have params table instead of flags
+  luaL_checktype(L, 4, LUA_TTABLE);
+
+  uint32_t flags = get_flags(L, 4);
 
   // use_int16_t is defined on top of compiled file
   // see luavox.c when it's built
   if (flags & SV_INIT_FLAG_AUDIO_INT16) {
     use_int16_t = 1;
-  } else {
+  } else if (flags & SV_INIT_FLAG_AUDIO_FLOAT32) {
     use_int16_t = 0;
   }
 
