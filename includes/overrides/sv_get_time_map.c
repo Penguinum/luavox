0eaff71fb128901804b399a4e1dd9638d3da90a8
Penguinum <penguinum@gmail.com>
2021-01-11 20:19:42 +0300

    Change how get_time_map works
    get_time_map now returns return code and time map as a Lua table

--- includes/overrides/sv_get_time_map.c
+++ includes/overrides/sv_get_time_map.c
@@ -1,3 +1,21 @@
+/*
+From SunVox docs:
+
+SV_TIME_MAP_SPEED: dest[X] = BPM | ( TPL << 16 ) (speed at the beginning of line X);
+SV_TIME_MAP_FRAMECNT: dest[X] = frame counter at the beginning of line X.
+*/
+static int get_time_map_flags(lua_State *L, int nparam) {
+  int flags = 0;
+
+  const char* dest = get_string_param(L, nparam, "dest");
+  if (strcmp(dest, "speed") == 0) {
+    flags |= SV_TIME_MAP_SPEED;
+  } else if (strcmp(dest, "framecnt") == 0) {
+    flags |= SV_TIME_MAP_FRAMECNT;
+  }
+  return flags;
+}
+
 static int lua_sv_get_time_map(lua_State* L) {
     int lua_slot = luaL_checkinteger(L, 1);
     int lua_start_line = luaL_checkinteger(L, 2);
@@ -5,7 +23,7 @@ static int lua_sv_get_time_map(lua_State* L) {
 
     uint32_t* lua_dest = (uint32_t*)calloc(lua_len, sizeof(uint32_t));
 
-    int lua_flags = luaL_checkinteger(L, 5);
+    int lua_flags = get_time_map_flags(L, 4);
 
     int lua_ret = sv_get_time_map(
       lua_slot,
@@ -14,15 +32,16 @@ static int lua_sv_get_time_map(lua_State* L) {
       lua_dest,
       lua_flags
     );
-    // lua stack: slot, line, len, {}, flags
+    lua_pushinteger(L, lua_ret);
+    // lua stack: slot, line, len, flags, lua_ret
+    lua_newtable(L); // time map
     for (int i = 0; i < lua_len; i++) {
       lua_pushinteger(L, i + 1);
       lua_pushinteger(L, lua_dest[i]);
-      // lua stack: slot, line, len, {}, flags, i, value
-      lua_settable(L, -4);
+      // lua stack: slot, line, len, flags, lua_ret, time_map, i, value
+      lua_settable(L, -3);
     }
-    lua_pushinteger(L, lua_ret);
     free(lua_dest);
-
-    return 1;
+    // lua stack: slot, line, len, flags, lua_ret, time_map
+    return 2;
   }
