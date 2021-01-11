b0ab874fa9edbbd3d9d6b9881e70ef2cd11df3d3
Penguinum <penguinum@gmail.com>
2021-01-11 19:46:39 +0300

    Moved util functions from sv_init.c to library top

--- build/luavox.c
+++ build/luavox.c
@@ -33,6 +33,33 @@ static uint32_t check_unsigned_int(lua_State *L, int arg) {
   return (uint32_t)ret;
 }
 
+static int is_flag_set_true(lua_State *L, int nparam, const char *key) {
+  lua_pushstring(L, key);
+  lua_gettable(L, nparam);
+  int ltype = lua_type(L, -1);
+  if (ltype == LUA_TNIL) {
+    return 0;
+  } else if (ltype == LUA_TBOOLEAN) {
+    return lua_toboolean(L, -1);
+  } else {
+    return 1;
+  }
+}
+
+static int get_int_param(lua_State *L, int nparam, const char *key) {
+  lua_pushstring(L, key);
+  lua_gettable(L, nparam);
+  int ltype = lua_type(L, -1);
+  if (ltype == LUA_TNIL) {
+    return 0;
+  } else if (ltype == LUA_TNUMBER) {
+    return luaL_checkinteger(L, -1);
+  } else {
+    luaL_error(L, "Bad parameter type for '%s': expected number, got %s\n", key, lua_typename(L, ltype));
+    return 0; // won't get there tbh
+  }
+}
+
 typedef struct Note {
   sunvox_note *note;
 } Note;
@@ -310,33 +337,6 @@ static int lua_sv_unlock_slot(lua_State *L) {
   lua_pushinteger(L, ret);
   return 1;
 }
-static int is_flag_set_true(lua_State *L, int nparam, const char *key) {
-  lua_pushstring(L, key);
-  lua_gettable(L, nparam);
-  int ltype = lua_type(L, -1);
-  if (ltype == LUA_TNIL) {
-    return 0;
-  } else if (ltype == LUA_TBOOLEAN) {
-    return lua_toboolean(L, -1);
-  } else {
-    return 1;
-  }
-}
-
-static int get_int_param(lua_State *L, int nparam, const char *key) {
-  lua_pushstring(L, key);
-  lua_gettable(L, nparam);
-  int ltype = lua_type(L, -1);
-  if (ltype == LUA_TNIL) {
-    return 0;
-  } else if (ltype == LUA_TNUMBER) {
-    return luaL_checkinteger(L, -1);
-  } else {
-    luaL_error(L, "Bad parameter type for '%s': expected number, got %s\n", key, lua_typename(L, ltype));
-    return 0; // won't get there tbh
-  }
-}
-
 /*
 From SunVox docs:
 
@@ -963,7 +963,7 @@ static const struct luaL_Reg luavox [] = {
   { "get_log", lua_sv_get_log},
   { "sunvox_buffer", new_buffer },
   { NULL, NULL }
-
+    
 };
 
 
